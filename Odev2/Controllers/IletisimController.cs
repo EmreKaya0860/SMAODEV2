021632979e594387e55c43b3611b5bea7a195ba0
EmreKaya0860 <h.emrekaya2207@gmail.com>
2022-11-09 22:16:32 +0300

    09.11.2022 Etüt

---
+++ Odev2/Controllers/IletisimController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Odev2.Controllers
+{
+    public class IletisimController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(IFormCollection collections)
+        {
+            ViewBag.Name = collections["_name"];
+            ViewBag.Email = collections["email"];
+            ViewBag.Mesaj = collections["mesaj"];
+            ViewBag.Check = collections["kontrol"][0];
+
+
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Odev2/Controllers/OgrendiklerimizController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Odev2.Controllers
+{
+    public class OgrendiklerimizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Asama2View()
+        {
+            return View();
+        }
+    }
+}
