4c2b81371be51366dabf42f6a5d44900eddfe60b
EmreKaya0860 <h.emrekaya2207@gmail.com>
2022-11-16 21:59:48 +0300

    16.11.2022 Etüt

--- Odev2/Controllers/IletisimController.cs
+++ Odev2/Controllers/IletisimController.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Odev2.Models;
+
 
 namespace Odev2.Controllers
 {
     public class IletisimController : Controller
     {
         [HttpPost]
-        public IActionResult Index(IFormCollection collections)
+        public IActionResult Index(Bilgiler[] bilgi)
         {
-            ViewBag.Name = collections["_name"];
-            ViewBag.Email = collections["email"];
-            ViewBag.Mesaj = collections["mesaj"];
-            ViewBag.Check = collections["kontrol"][0];
 
+            //ViewBag.Name = collections["_name"];
+            //ViewBag.Email = collections["email"];
+            //ViewBag.Mesaj = collections["mesaj"];
+            //ViewBag.Check = collections["kontrol"][0];
 
-            return View();
+            if (ModelState.IsValid) // Eğer Model Nesnesi (uye) Validasyon Kuralları Geçerli ise
+            {
+                // Kurallara uyulmuşsa uye nesnesini veritabanına kaydet
+            }
+
+            if (!ModelState.IsValid) // Model Durumu Geçersiz ise
+            {
+                // Kullanıcıya hata mesajı göster
+                ModelState.AddModelError("", "Lütfen Zorunlu Alanları Doldurunuz"); // Bu metotla ekrana kendi hata mesajlarımızı gönderebiliriz
+            }
+
+            return View(bilgi);
+
+            
         }
 
         public IActionResult Index()
---
+++ Odev2/Models/Bilgiler.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Odev2.Models
+{
+    public class Bilgiler
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} alanını boş geçemezsiniz")]
+        public string IsimSoyisim { get; set; }
+
+        [Required(ErrorMessage = "{0} alanını boş geçemezsiniz")]
+        [EmailAddress(ErrorMessage = "Yanlış E posta Biçimi")]
+        public string Eposta { get; set; }
+        [Required(ErrorMessage = "{0} alanını boş geçemezsiniz")]
+
+        [StringLength(500)]
+        public string Mesaj { get; set; }
+
+        [Phone]
+        public string? Telefon { get; set; }
+
+        [Required(ErrorMessage = "{0} alanını boş geçemezsiniz")]
+
+
+        public string TcKimlikNo { get; set; }
+    }
+}
